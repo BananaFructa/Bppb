c945cba47c9ae58bb84d6b0c1e7a59fc5d53fe3f
Banana Fructa <55049439+BananaFructa@users.noreply.github.com>
2020-12-13 14:11:31 +0200

    removed check for response

--- Bppb.ts
+++ Bppb.ts
@@ -37,26 +37,24 @@ function Predict() {
 
     AI.PredictFromTensor("INPUT_"+JSON.stringify(InputTensor),Response => {
 
-        if (Response === "EXEC_1") {
 
-            let OutputTensor = JSON.parse(fs.readFileSync("./PythonOutput.json").toString().replace(/ /g,"").replace(/\n/g,""));
-            let PredictedPrices = [];
-            for (let i : number = 0;i < Items.length;i++) {
-                PredictedPrices[i] = [];
-                PredictedPrices[i][BUY_INDEX] = [];
-                PredictedPrices[i][SELL_INDEX] = [];
-                PredictedPrices[i][BUY_INDEX][0] = InputTensor[i][BUY_INDEX][20];
-                PredictedPrices[i][SELL_INDEX][0] = InputTensor[i][SELL_INDEX][20];
-                // calculated the prices based on the given derivatives
-                for (let j : number = 0;j < 19;j++) { // there are 19 points in the prediction
-                    PredictedPrices[i][BUY_INDEX][j+1] = PredictedPrices[i][BUY_INDEX][j] + OutputTensor[i][BUY_INDEX][j]; 
-                    PredictedPrices[i][SELL_INDEX][j+1] = PredictedPrices[i][SELL_INDEX][j] + OutputTensor[i][SELL_INDEX][j]; 
-                }
+        let OutputTensor = JSON.parse(fs.readFileSync("./PythonOutput.json").toString().replace(/ /g,"").replace(/\n/g,""));
+        let PredictedPrices = [];
+        for (let i : number = 0;i < Items.length;i++) {
+            PredictedPrices[i] = [];
+            PredictedPrices[i][BUY_INDEX] = [];
+            PredictedPrices[i][SELL_INDEX] = [];
+            PredictedPrices[i][BUY_INDEX][0] = InputTensor[i][BUY_INDEX][20];
+            PredictedPrices[i][SELL_INDEX][0] = InputTensor[i][SELL_INDEX][20];
+            // calculated the prices based on the given derivatives
+            for (let j : number = 0;j < 19;j++) { // there are 19 points in the prediction
+                PredictedPrices[i][BUY_INDEX][j+1] = PredictedPrices[i][BUY_INDEX][j] + OutputTensor[i][BUY_INDEX][j]; 
+                PredictedPrices[i][SELL_INDEX][j+1] = PredictedPrices[i][SELL_INDEX][j] + OutputTensor[i][SELL_INDEX][j]; 
             }
-
-            fs.writeFileSync("./Prediction.json",JSON.stringify(PredictedPrices));
-            console.log("Initial Prediction was Runned");
         }
+
+        fs.writeFileSync("./Prediction.json",JSON.stringify(PredictedPrices));
+        console.log("Initial Prediction was Runned");
 
     });
 }
